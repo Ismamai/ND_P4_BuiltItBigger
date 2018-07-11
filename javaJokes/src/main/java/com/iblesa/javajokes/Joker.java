5ba10b4c4521d893e9c94d2b110fc62806156953
iblesa <isma.work@gmail.com>
2018-07-11 20:34:24 +0100

    Implementation with some "jokes" from https://short-funny.com

--- javaJokes/src/main/java/com/iblesa/javajokes/Joker.java
+++ javaJokes/src/main/java/com/iblesa/javajokes/Joker.java
@@ -1,4 +1,36 @@
 package com.iblesa.javajokes;
 
+import java.util.Random;
+
 public class Joker {
+    private String[] jokes = new String[]
+
+            {
+                    "Can a kangaroo jump higher than a house? \n" +
+                            "Of course, a house doesn’t jump at all.",
+
+                    "Doctor: \"I'm sorry but you suffer from a terminal illness and have only 10 to live.\"\n" +
+                            "Patient: \"What do you mean, 10? 10 what? Months? Weeks?!\"\n" +
+                            "Doctor: \"Nine.\"",
+
+                    " Anton, do you think I’m a bad mother?\n" +
+                            "My name is Paul.",
+
+                    "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
+
+                    "What is the difference between a snowman and a snowwoman?\n" +
+                            "-\n" +
+                            "Snowballs."
+
+            };
+    private Random random;
+
+    public Joker() {
+        random = new Random();
+    }
+
+    public String getJoke() {
+        int nextJoke = random.nextInt(jokes.length);
+        return jokes[nextJoke];
+    }
 }
